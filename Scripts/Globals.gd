7541c50bfc24af2c197127ffa71f796409c3ef51
Ryan Siegel-Stechler <rsiegelstechler@sparkypants.com>
2025-08-01 14:19:35 -0400

    Fixing font size on High Scores for console export; finishing touches!

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -39,26 +39,26 @@ func return_to_menu() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 func high_score_text() -> String:
-	return str("[font_size=50][u]High Scores[/u]\n\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"[/font_size]")
+	return str("[font_size=40][u]High Scores[/u]\n\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"[/font_size]")
 
 func new_high_score_text(input_letter : String = "") -> String:
 	var name_string : String = high_score_names[replacement_score]
 	if input_letter == "":
-		return str("[font_size=50][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
+		return str("[font_size=40][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
 	elif input_letter == "backspace":
 		if name_string != "":
 			name_string = name_string.erase(name_string.length()-1, 1)  
 			high_score_names[replacement_score] = name_string
-			return str("[font_size=50][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
+			return str("[font_size=40][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
 		else:
-			return str("[font_size=50][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
+			return str("[font_size=40][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
 	else:
 		if name_string.length() < 3:
 			name_string += input_letter
 			high_score_names[replacement_score] = name_string
-			return str("[font_size=50][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
+			return str("[font_size=40][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
 		else:
-			return str("[font_size=50][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
+			return str("[font_size=40][u]High Scores[/u]\n[rainbow freq=.2 sat=.8 val=.8][wave amp=20 freq=20]New High Score![/wave][/rainbow]\n1: ",high_scores[0]," - ",high_score_names[0],"\n2: ",high_scores[1]," - ",high_score_names[1],"\n3: ",high_scores[2]," - ",high_score_names[2],"\n4: ",high_scores[3]," - ",high_score_names[3],"\n5: ",high_scores[4]," - ",high_score_names[4],"\n\nInput your initials; press Enter when done.[/font_size]")
 		
 func show_high_scores():
 	var temp_score : int
